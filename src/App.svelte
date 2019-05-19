61d97f216cfdd546d951e87beeb7e5f35cb4688b
joyjoe <plachy.jozef@gmail.com>
2019-05-20 01:02:50 +0200

    adding variables and button

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,22 @@
 <script>
   export let name;
+  export let foo;
+  let money = 37;
+
+  function incrementMoney() {
+    money += 1;
+  }
 </script>
 
 <style>
   h1 {
     color: purple;
   }
+  h2 {
+    color: blueviolet
+  }
 </style>
 
-<h1>Hello {name}</h1>
+<h1>Hello!</h1>
+<h2>My name is {name}, and I have {money} million $$!</h2>
+<button on:click="{incrementMoney}">Add Million</button>
